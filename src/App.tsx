93ac20a22bcb027638a19a6ed40607ee41a6931f
EmeraldPangolin <meyerdeavan29205@gmail.com>
2023-04-27 23:31:02 -0500

    Added Home components and cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
+
+const Home =() => (
+  <>
+    <h1>Connect 4</h1>
+    <h2>Companion App</h2>
+    <Button 
+      variant="outline-primary">Play
+    </Button>
+    <Card>
+      <Card.Header>
+        Leaderboard
+      </Card.Header>
+      <Card.Body>
+        Play a game to see your leaderboard...
+      </Card.Body>
+    </Card>
+  </>
+);
 
 function App() {
   return (
