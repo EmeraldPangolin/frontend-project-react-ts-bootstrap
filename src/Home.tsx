1ce428177aa6b594e421a45344e82f9441281823
EmeraldPangolin <meyerdeavan29205@gmail.com>
2023-04-28 00:28:12 -0500

    setting up nav

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,20 +1,25 @@
 import { Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 export const Home =() => {
+
+    const nav = useNavigate();
+
     return(
         <>
-        <Button 
-            variant="outline-primary">Play
-        </Button>
-        <Card className="mt-3">
-            <Card.Header>
-            Leaderboard
-            </Card.Header>
-            <Card.Body>
-            Play a game to see your leaderboard...
-            </Card.Body>
-        </Card>
+            <Button 
+                variant="outline-primary"
+                onClick={() => nav("/setup")}>Play
+            </Button>
+            <Card className="mt-3">
+                <Card.Header>
+                Leaderboard
+                </Card.Header>
+                <Card.Body>
+                Play a game to see your leaderboard...
+                </Card.Body>
+            </Card>
         </>
   )
 };
